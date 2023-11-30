efbac04ab5da6c1cd34c6003d5c0308470cad526
Emre Hakan Erdemir <92296896+erdemire21@users.noreply.github.com>
2023-11-30 15:04:36 +0000

    added predictions

--- DecisionTree.py
+++ DecisionTree.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class Node():
-    def __init__(self, feature, left,right, information_gain, entropy, data):
+    def __init__(self, feature=None, left=None, right=None, information_gain=None, entropy=None, data=None):
         self.feature = feature
         self.left = left
         self.right = right
@@ -28,9 +28,12 @@ class DecisionTree():
 
         for i in range(no_of_labels):
             p = len(y_values[y_values==i]) / len(y_values)
-            entropy += -p * math.log(p, 2)
-
+            if p != 0:
+                entropy += -p * math.log(p, 2)
+            else:
+                entropy += 0
         return entropy
+
 
     def information_gain(self,parent,left,right):
 
@@ -41,7 +44,11 @@ class DecisionTree():
         return self.entropy(parent) - (left_weight * self.entropy(left) + right_weight * self.entropy(right))
 
 
-    # Function to find the best split
+    def fit(self, X, y):
+        dataset = np.concatenate((X, y), axis=1)
+        self.root = self.create_tree(dataset)
+
+    
     def best_split(self, dataset):
 
         best_split = {}
@@ -53,36 +60,52 @@ class DecisionTree():
                 left_data = np.array([i for i in dataset if i[feature] <= value])
                 right_data = np.array([i for i in dataset if i[feature] > value])
 
-            if len(left_data) > 0 and len(right_data) > 0:
-                y = dataset[:, -1]
-                left_y = left_data[:, -1]
-                right_y = right_data[:, -1]
-                info_gain = self.information_gain(y, left_y, right_y)
+                if len(left_data) > 0 and len(right_data) > 0:
+                    y = dataset[:, -1]
+                    left_y = left_data[:, -1]
+                    right_y = right_data[:, -1]
+                    info_gain = self.information_gain(y, left_y, right_y)
 
-                if info_gain > best_info_gain:
-                    best_split['feature'] = feature
-                    best_split['value'] = value
-                    best_split['left'] = left_data
-                    best_split['right'] = right_data
-                    best_split['info_gain'] = info_gain
-                    best_info_gain = info_gain
+                    if info_gain > best_info_gain:
+                        best_split['feature'] = feature
+                        best_split['data'] = value
+                        best_split['left'] = left_data
+                        best_split['right'] = right_data
+                        best_split['info_gain'] = info_gain
+                        best_info_gain = info_gain
         return best_split
 
 
-    def create_tree(self,dataset,counter=0,max_depth=5):
+    def create_tree(self,dataset,counter=0,max_depth=3):
 
-        if max_depth <= counter or dataset[:, -1] >= self.min_samples_split:
-            
+        if max_depth >= counter and len(dataset[:, -1]) >= self.min_samples_split:
+
+
             best_split = self.best_split(dataset)
 
             if best_split['info_gain'] > 0:
                 left = self.create_tree(best_split['left'], counter + 1, self.max_depth)
                 right = self.create_tree(best_split['right'], counter + 1, self.max_depth)
                 return Node(best_split['feature'], left, right, best_split['info_gain'],
-                             self.entropy(dataset[:, -1]))
+                             self.entropy(dataset[:, -1]),best_split['data'])
             
             # Return the leaf node with the majority class if threshold is reached
             Y = dataset[:, -1]
             leaf_value = max(Y, key=list(Y).count)
             return Node(data=leaf_value)
         
+
+    def predict(self, X,tree):
+        if tree.data != None:
+            return tree.data
+        feature_value = X[tree.feature]
+        if feature_value <= tree.data:
+            return self.predict(X,tree.left)
+        else:
+            return self.predict(X,tree.right)
+    
+    def predictions(self, X):
+        predictions = []
+        for row in X:
+            predictions.append(self.predict(row,self.root))
+        return predictions
