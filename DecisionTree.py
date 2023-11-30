046079b3dd5803346d37fcc8fd3441d722c35627
Emre Hakan Erdemir <92296896+erdemire21@users.noreply.github.com>
2023-11-30 11:37:22 +0000

    added best split

--- DecisionTree.py
+++ DecisionTree.py
@@ -41,4 +41,29 @@ class DecisionTree():
         return self.entropy(parent) - (left_weight * self.entropy(left) + right_weight * self.entropy(right))
 
 
-        
+    # Function to find the best split
+    def best_split(self, dataset):
+
+        best_split = {}
+        best_info_gain = -1000
+        # Loop through all the features
+        for feature in range(dataset.shape[1]-1):
+            # Loop through all the unique values of the feature
+            for value in np.unique(dataset[:, feature]):
+                left_data = np.array([i for i in dataset if i[feature] <= value])
+                right_data = np.array([i for i in dataset if i[feature] > value])
+
+            if len(left_data) > 0 and len(right_data) > 0:
+                y = dataset[:, -1]
+                left_y = left_data[:, -1]
+                right_y = right_data[:, -1]
+                info_gain = self.information_gain(y, left_y, right_y)
+
+                if info_gain > best_info_gain:
+                    best_split['feature'] = feature
+                    best_split['value'] = value
+                    best_split['left'] = left_data
+                    best_split['right'] = right_data
+                    best_split['info_gain'] = info_gain
+                    best_info_gain = info_gain
+        return best_split
