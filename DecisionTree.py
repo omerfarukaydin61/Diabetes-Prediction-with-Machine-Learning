f22b2ff90f8958f0bcd453b502fa740bf9eb25b5
Emre Hakan Erdemir <92296896+erdemire21@users.noreply.github.com>
2023-11-30 10:42:48 +0000

    started from scratch, created node,dtree entropy

--- DecisionTree.py
+++ DecisionTree.py
@@ -13,51 +13,24 @@ class Node():
         self.data = data
 
 
-class DecisionTree(object):
+class DecisionTree():
 
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-        self.tree = self.build_decision_tree(data, labels)
+    def __init__(self, min_samples_split=2,max_depth = 5):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
 
-    def entropy(self,choice1,choice2):
+    def entropy(self,y_values):
+        # Data is numpy array
         
-        choice1_entropies = []
-        choice2_entropies = []
+        entropy = 0
+        # our labels are either 1 or 0
+        no_of_labels = 2
 
+        for i in range(no_of_labels):
+            p = len(y_values[y_values==i]) / len(y_values)
+            entropy += -p * math.log(p, 2)
 
-        if len(choice1) != 0:
-            for i in range(len(choice1)):
-                label1 = choice1[i].count(1)
-                label0 = choice1[i].count(0)
-                if label1 != 0 and label0 != 0:
-                    choice1_entropies.append(-((label1/len(choice1[i]))*math.log2(label1/len(choice1[i])))-((label0/len(choice1[i]))*math.log2(label0/len(choice1[i]))))
-                else:
-                    choice1_entropies.append(0)
-        if len(choice2) != 0:
-            for i in range(len(choice2)):
-                label1 = choice2[i].count(1)
-                label0 = choice2[i].count(0)
-                if label1 != 0 and label0 != 0:
-                    choice2_entropies.append(-((label1/len(choice2[i]))*math.log2(label1/len(choice2[i])))-((label0/len(choice2[i]))*math.log2(label0/len(choice2[i]))))
-                else:
-                    choice2_entropies.append(0)
-
-        return choice1_entropies, choice2_entropies
-    
-    def information_gain(self,parent,children):
-
-        info_gain = 0
-
-        if len(children) != 0:
-            for i in range(len(children)):
-                info_gain += (len(children[i])/len(parent))*self.entropy([children[i]],[children[i]])[0][0]
-        
-
-        info_gain = self.entropy([parent],[parent])[0][0] - info_gain
-
-        return info_gain
-
+        return entropy
 
 
 
