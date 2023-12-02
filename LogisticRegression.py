0ab7ca5d51cfd99eb978a53f8ba1c821292e19c1
Ömer Faruk Aydın <omerfarukaydin0261@gmail.com>
2023-12-02 17:57:31 +0300

    Update LogisticRegression.py

---
+++ LogisticRegression.py
@@ -0,0 +1,61 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=1000):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.weights = None
+        self.bias = None
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def forward(self, X):
+        # Calculate the predicted values
+        z = np.dot(X, self.weights) + self.bias
+        y_pred = self.sigmoid(z)
+        return y_pred
+
+    def backward(self, X, y, y_pred):
+        m = len(y)
+        
+        # Calculate gradients
+        dz = y_pred - y
+        dw = (1/m) * np.dot(X.T, dz)
+        db = (1/m) * np.sum(dz)
+
+        # Update weights and bias
+        self.weights -= self.learning_rate * dw
+        self.bias -= self.learning_rate * db
+
+    def fit(self, X, y):
+        m, n = X.shape
+        self.weights = np.zeros(n)
+        self.bias = 0
+
+        for _ in range(self.num_iterations):
+            # Forward pass
+            y_pred = self.forward(X)
+
+            # Backward pass
+            self.backward(X, y, y_pred)
+
+    def predict(self, X):
+        # Make predictions using the learned weights and bias
+        z = np.dot(X, self.weights) + self.bias
+        y_pred_prob = self.sigmoid(z)
+        # Round the values to 0 or 1
+        y_pred = np.round(y_pred_prob)
+        return y_pred
+
+    def confusion_matrix(self, y_true, y_pred):
+        return confusion_matrix(y_true, y_pred)
+    
+    def metrics(self, confusion_matrix):
+        tn, fp, fn, tp = confusion_matrix.ravel()
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1_score = 2 * precision * recall / (precision + recall)
+        return accuracy, precision, recall, f1_score
