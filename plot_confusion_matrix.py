1743ad0526e554738f14b666d7f3c827501b22fd
Ömer Faruk Aydın <omerfarukaydin0261@gmail.com>
2023-12-04 00:29:05 +0300

    Updated plot_confusion_matrix.py and project2.ipynb

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def PlotConfusinMatrix(confusion_matrix, title):
+def plot_confusion_matrix(confusion_matrix, title):
     plt.figure(figsize=(10,6))
     confusion_matrix[0][0], confusion_matrix[1][1] = confusion_matrix[1][1], confusion_matrix[0][0]
     confusion_matrix[0][1], confusion_matrix[1][0] = confusion_matrix[1][0], confusion_matrix[0][1]
