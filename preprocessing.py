65d7032cc806a04b981204da327eb2dafa770a03
Emre Hakan Erdemir <92296896+erdemire21@users.noreply.github.com>
2023-12-31 14:21:08 +0000

    created preprocessing.py and added 2 preprocessing options

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+def replace_zeros_with_median_considering_outcome(data, columns_to_fill = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']):
+
+
+    def median_target(var):
+        temp = data[data[var].notnull()]
+        temp = temp[[var, 'Outcome']].groupby(['Outcome'])[[var]].median().reset_index()
+        return temp
+
+    # Setting 0 values to NaN in the given comlumns
+    data[columns_to_fill] = data[columns_to_fill].replace(0, pd.NA)
+
+    # Replacing NaN values with median based on 'Outcome'
+    for column in columns_to_fill:
+        median_values = median_target(column)
+        for index, row in median_values.iterrows():
+            outcome_value = row['Outcome']
+            median_value = row[column]
+            data.loc[(data[column].isna()) & (data['Outcome'] == outcome_value), column] = median_value
+
+    return data
+
+
+def replace_zeros_with_median(data, columns_to_fill = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']):
+
+    data[columns_to_fill] = data[columns_to_fill].replace(0, pd.NA)
+
+    data[columns_to_fill] = data[columns_to_fill].apply(lambda x: x.fillna(x.median()))
+
+    return data
+
+
